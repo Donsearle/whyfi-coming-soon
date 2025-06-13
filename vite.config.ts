6428ee770123e5cf4dd1ca58f0d4f81eac4f18f1
DonWhyFi <donovan@whyfi.co.za>
2025-06-13 19:45:49 +0200

    git

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    dedupe: ['react', 'react-dom'],
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      fs: 'fs',
+      '@': path.resolve(__dirname, './src')
+    }
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    include: ['lucide-react']
+  }
 });
